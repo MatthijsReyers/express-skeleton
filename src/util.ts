f8f491f9d1e6d6182a2439869a1416148b62160f
WebWolfy <46201442+MatthijsReyers@users.noreply.github.com>
2021-03-28 06:05:03 +0200

    Added timing attack safe string compare funtion

--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,15 @@ import { Request, Response } from 'express';
 export function redirect(url: string) {
     return (req: Request, res: Response) => res.redirect(url);
 }
+
+/**
+ * Timing attack safe string comparision function that intentionally has no 
+ * optimizations and should always take the same amount of time to run.
+ */
+ export function safeStringCompare(hash1:string, hash2:string): boolean
+ {
+     let check = (hash1.length === hash2.length);
+     for (let i = 0; i < hash1.length; i++)
+         check = check && (hash1[i] === hash2[i]);
+     return check;
+ }
